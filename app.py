ddb46fec82e6f50826c2893b33972e60ae2be04b
mortelnik <mortelo@yandex.com>
2025-12-02 16:47:03 +1200

    Домашка для Михаила

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/status_check', methods=['GET'])
+def get_status():
+    data = {
+        "status": "OK",
+        "service": "Мой или мое API",
+        "version": "1.0.0"
+    }
+    return jsonify(data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
